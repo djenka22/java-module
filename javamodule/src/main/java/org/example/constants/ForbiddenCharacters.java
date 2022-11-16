cf359389f2582dc0d1aab576f467671773b5fc24
Nikola <nikola.rusimovic@gmail.com>
2022-11-16 16:30:50 +0100

    some fixes, code coverage 79%

--- javamodule/src/main/java/org/example/constants/ForbiddenCharacters.java
+++ javamodule/src/main/java/org/example/constants/ForbiddenCharacters.java
@@ -1,6 +1,5 @@
 package org.example.constants;
 
-public interface ForbiddenCharacters {
-    String  FORBIDDEN_CHARACTERS = "čćšđžĐŽĆČŠ";
-
+public class ForbiddenCharacters {
+    public static final String  FORBIDDEN_CHARACTERS = "čćšđžĐŽĆČŠ";
 }
